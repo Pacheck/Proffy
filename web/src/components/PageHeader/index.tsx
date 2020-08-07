a9b5d2800fa8982afd56e163302b7a18939eadc4
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-07 18:21:09 -0300

    creating / updating components

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,10 +8,12 @@ import './styles.css';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FunctionComponent<PageHeaderProps> = ({
   title,
+  description,
   children,
 }) => {
   return (
@@ -25,6 +27,7 @@ const PageHeader: React.FunctionComponent<PageHeaderProps> = ({
 
       <div className="header-content">
         <strong>{title}</strong>
+        {description && <p>{description}</p>}
         {children}
       </div>
     </header>
