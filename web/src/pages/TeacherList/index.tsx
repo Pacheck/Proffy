7fbbf884a77368298ce86701bc9a891b28ba94e4
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-07 18:21:59 -0300

    importing useState / componentizate

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
+import Select from '../../components/Select';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+
 import './styles.css';
 
 function TeacherList() {
@@ -10,20 +12,32 @@ function TeacherList() {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="subject">Dia da semana</label>
-            <input type="text" id="week_day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="subject">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Biologias', label: 'Biologias' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Química', label: 'Química' },
+              { value: 'Física', label: 'Física' },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda' },
+              { value: '2', label: 'Terça' },
+              { value: '3', label: 'Quarta' },
+              { value: '4', label: 'Quinta' },
+              { value: '5', label: 'Sexta' },
+              { value: '6', label: 'Sábado' },
+            ]}
+          />
+          <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
 
--- web/src/pages/landing/index.tsx
+++ web/src/pages/landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -7,10 +7,19 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('/connections').then((response) => {
+      console.log(response);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -36,10 +45,17 @@ function Landing() {
           </Link>
         </div>
 
-        <span className="total-connections">
-          Total de 200 conexões já realizadas{' '}
-          <img src={purpleHeartIcon} alt="Coração roxo"></img>
-        </span>
+        {totalConnections ? (
+          <span className="total-connections">
+            Total de 200 conexões já realizadas
+            <img src={purpleHeartIcon} alt="Coração roxo"></img>
+          </span>
+        ) : (
+          <span className="total-connections">
+            Você ainda não possui nenhuma conexão realizada
+            <img src={purpleHeartIcon} alt="Coração roxo"></img>
+          </span>
+        )}
       </div>
     </div>
   );
